4bdd4074bf836f12b46dda31ff7942d06f4ff138
Mayank Bhamre <mayankbhamre@gmail.com>
2021-06-20 22:48:58 +0530

    Small changes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,61 +3,8 @@ import axios from "./../axios"
 import requests from "./../Request"
 import './Banner.css'
 
-// const styles ={
-// banner:{
-//     position:"relative",
-//     color:"white",
-//     objectFit:"contain",
-//     width:"100%",
-//     height:"448px",
-//     "& .banner_contents":{
-//         marginLeft:"30px",
-//         paddingTop:"140px",
-//         height:"190px",
-//         "& .banner_title":{
-//             fontSize:"3rem",
-//             fontWeight:800,
-//             paddingBottom:"0.3rem"
-//         },
-//         "& .banner_description":{
-//             width:"45rem",
-//             lineHeight:1.3,
-//             paddingTop:"1rem",
-//             fontSize:"0.8rem",
-//             maxWidth:"360px",
-//             height:"80px"
-//         }
-//     },
-//     "& .banner_button":{
-// cursor:"pointer",
-// color:"#fff",
-// outline:"none",
-// border:"none",
-// fontWeight:700,
-// borderRadius:"0.2vw",
-// paddingLeft:"2rem",
-// paddingRight:"2rem",
-// marginRight:"1rem",
-// paddingTop:"0.5rem",
-// backgroundColor:"rgba(51,51,51,0.5)",
-// paddingBottom:"0.5rem",
-// "&:hover":{
-//     color:"#000",
-//     backgroundColor:"#e6e6e6",
-//     transition:"all 0.2s"
-// }
-
-//     },
-//     "& .banner--fadeBottom":{
-//         height:"7.4rem",
-//         backgroundImage:"linear-gradient(180deg,transparent,rgba(37,37,37,0.61),#111)"
-//     }
-
-// }
-// }
 
 function Banner(props) {
-    // const {classes} = props
     const [movie, setMovie] = useState([])
 
     useEffect(()=>{
